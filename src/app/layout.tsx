4f19b3491242c8ddfe75b7bc4006848604d23a58
ryxxuk <81872593+ryxxuk@users.noreply.github.com>
2023-06-25 21:05:21 +0100

    added gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @next/next/no-script-component-in-head */
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +20,30 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<Head>
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+				})(window,document,'script','dataLayer','GTM-PVM3MP2');
+      `}
+				</Script>
+			</Head>
 			<body className={inter.className}>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-PVM3MP2"
+						height="0"
+						width="0"
+						style={{ display: "none", visibility: "hidden" }}
+					></iframe>
+				</noscript>
+
 				<Navbar />
 				{children}
-				<Footer/>
+				<Footer />
 			</body>
 		</html>
 	);
